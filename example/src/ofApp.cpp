2b150eaa5f45506bb0e0db4db8b137a6aa32c538
Kenta Watashima <kenta@prty.jp>
2016-03-24 20:08:53 +0900

    Update example

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -9,11 +9,19 @@ public:
     
     void setup() {
         ofBackground(0);
-        ofSetFrameRate(30);
+        ofSetFrameRate(60);
         
         font.load("verdana.ttf", 12);
-        text[0].setup(font, OF_TEXT, false, 1.f);
-        text[1].setup(font, OF_TEXT, true, 1.f);
+        text[0].setup(font, OF_TEXT);
+        text[1].setup(font, OF_TEXT);
+        
+        text[0].setRandomChars(OFX_SHUFFLE_TEXT_NUMBERS
+                               | OFX_SHUFFLE_TEXT_UPPER_LETTERS
+                               | OFX_SHUFFLE_TEXT_LOWER_LETTERS
+                               | OFX_SHUFFLE_TEXT_SYMBOLS);
+        
+        text[1].setRandomChars(OFX_SHUFFLE_TEXT_UPPER_LETTERS
+                               | OFX_SHUFFLE_TEXT_LOWER_LETTERS); // only letters
     }
 
     void update() {
@@ -29,8 +37,8 @@ public:
     
     void keyPressed(int key) {
         if (key == ' ') {
-            !text[0].isRunning() ? text[0].start() : text[0].stop();
-            !text[1].isRunning() ? text[1].start() : text[1].stop();
+            !text[0].isRunning() ? text[0].start(1.f, 0, false) : text[0].stop();
+            !text[1].isRunning() ? text[1].start(1.f, 0, true) : text[1].stop();
         }
     }
 };
